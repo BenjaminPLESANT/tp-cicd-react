aeeb2616ec7bd426522c983d197c0e3c86eee30d
BenjaminPLESANT <benjamin.plesant@outlook.fr>
2022-09-21 14:02:09 +0200

    Test cicd build automatique à chaque push

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Hello World !</h1>
+        <h1>Hello World test cicd !</h1>
         <p>Welcome to the CICD TP</p>
         <p>This app was made with react and deploy with Netlify</p>
       </header>
