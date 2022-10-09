420558b26056961d961d959686b161967c8f1265
BenjaminPLESANT <benjamin.plesant@outlook.fr>
2022-10-09 17:11:18 +0200

    Change text

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
       <header className="App-header">
         <h1>Hey, i'm Benjamin !</h1>
         <small>Software Engineer in progess...</small>
-        <p>Welcome to the CICD TP, this app was made with react and deploy with Netlify</p>
-        <p>The objective was to create a workflow that builds the application and deploys it on Netlify with each github push</p>
+        <p>Welcome to the CICD TP, this app was made with react and deploy with Netlify.</p>
+        <p>The objective was to create a workflow that builds the application and deploys it on Netlify with each github push.</p>
       </header>
     </div>
   );
